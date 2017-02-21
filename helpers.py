cb5771af61e481825f5f4ca8eb67465b79bdefb1
Andre Tan <tanandre93@gmail.com>
2017-02-20 22:36:43 -0800

    modify save csv method

--- helpers.py
+++ helpers.py
@@ -1,21 +1,19 @@
 import csv
 import itertools
 
+RESULTS = 'results/{}'
+
 def save_logs(filename, contents):
     with open(RESULTS.format(filename), 'w') as f:
         f.write(contents)
 
-def save_report(params, results):
-    filepath = RESULTS.format('results.csv')
-    initial_state, length, (a,b) = params
-    if not os.path.exists(filepath):
-        with open(filepath, 'w') as f:
-            w = csv.writer(f)
-            w.writerow(['i','T','a','b','frac_state1','frac_1to2','frac_2to1','pi1','pi2'])
-    r1, r2, r3, pi1, pi2 = results
-    with open(filepath, 'a') as f:
+def save_report(filename, headers, results):
+    filepath = RESULTS.format(filename + '.csv')
+    with open(filepath, 'w') as f:
         w = csv.writer(f)
-        w.writerow([initial_state, length, a, b, r1, r2, r3, pi1, pi2])
+        w.writerow(headers)
+        for result in results:
+            w.writerow(result)
 
 def pairwise(iterable):
     a, b = itertools.tee(iterable)
